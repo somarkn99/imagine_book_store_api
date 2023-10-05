d17d3a849e34d4c506d73cb9cb44283598515be3
Somar <kesen.somar.99@gmail.com>
2023-10-05 13:07:55 +0300

    Publish lang files

---
+++ lang/en/books.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'title_required' => 'The title field is required.',
+    'title_max' => 'The title may not be greater than 255 characters.',
+    'cover_required' => 'The cover field is required.',
+    'cover_image' => 'The cover must be an image.',
+    'cover_mimes' => 'The cover must be in JPEG or PNG format.',
+    'author_required' => 'The author field is required.',
+    'author_max' => 'The author may not be greater than 255 characters.',
+    'genre_required' => 'The genre field is required.',
+    'genre_max' => 'The genre may not be greater than 255 characters.',
+    'price_required' => 'The price field is required.',
+    'price_numeric' => 'The price must be a numeric value.',
+    'price_min' => 'The price must be at least 0.',
+    'stock_required' => 'The stock field is required.',
+    'stock_integer' => 'The stock must be an integer.',
+    'stock_min' => 'The stock must be at least 0.',
+
+];
---
+++ lang/en/general.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'delete' => 'Data Deleted Successfully.',
+    'store' => 'Data Stored Successfully.',
+    'get' => 'Data fetched Successfully.',
+    'update' => 'Data Updated Successfully.',
+    'notCompleteData' => 'User need to complete Profile.',
+    'notAllowed' => 'User Not Allowed to do this Action.',
+];
